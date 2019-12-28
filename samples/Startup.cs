b44e218c30e0109d865ae385356b1143418ef1e4
Dean Marcussen <dean.marcussen@gmail.com>
2019-12-28 12:06:42 +0000

    Upgrade core 3.1 and ci  (#2)
    * Upgrade to core 3.1. Add setup event handler to configure automatically, and restrict recipes to only that of TheBlogTheme
    
    * Add some nuget tags
    
    * add gitversion.yml
    
    * Tinker gitversion
    
    * tweak
    
    * update gitversion

--- samples/Startup.cs
+++ samples/Startup.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
+using OrchardCore.Recipes.Services;
+using OrchardCore.Setup.Events;
+using System.Linq;
 
 namespace ThisNetWorks.OrchardCore.GoogleMaps.Sample
 {
@@ -9,9 +11,16 @@ namespace ThisNetWorks.OrchardCore.GoogleMaps.Sample
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddOrchardCms();
+            services.AddOrchardCms(builder => builder
+                .ConfigureServices(services => {
+                    services.AddScoped<ISetupEventHandler, SetupGoogleMapsSampleSiteEvent>();
+                    var harvester = services.FirstOrDefault(x => x.ServiceType == typeof(IRecipeHarvester) && x.ImplementationType == typeof(RecipeHarvester));
+                    services.Remove(harvester);
+                    services.AddScoped<IRecipeHarvester, RestrictedRecipeHarvestor>();
+                }, 100)
+            );
         }
-        
+
         public void Configure(IApplicationBuilder app, IHostEnvironment env)
         {
             if (env.IsDevelopment())
